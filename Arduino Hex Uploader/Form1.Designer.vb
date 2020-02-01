34380ec434be4db9099fbba1582995fe49fd126f
Joshua Glass <joshuag1000@outlook.com>
2020-02-01 16:38:52 +0000

    Form1 now inherits material skin

--- Arduino Hex Uploader/Form1.Designer.vb
+++ Arduino Hex Uploader/Form1.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
-    Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
